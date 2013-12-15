7097699cbd5e49e586fd80cf9f4e4cb698dfbee4
Cesar Lopez-Nataren <cesarn@mindtouch.com>
2013-12-15 08:45:42 -0800

    Add initial file

---
+++ src/App.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
+import Web.Scotty
+import Data.Aeson ((.:), (.:?), decode, FromJSON(..), Value(..))
+import Control.Applicative ((<$>), (<*>))
+import qualified Data.ByteString.Lazy.Char8 as BS
+import Network.HTTP.Types
+import Network.Wai.Middleware.RequestLogger
+
+main = scotty 3000 $ do
+  middleware logStdoutDev
+  
+  get "/status" $ do
+    json [(0::Int) .. 10]
+
+  post "/notify" $ do
+    status status200
+    json ("notified" :: String)
+  
+  notFound $ do 
+    json ("there is no such route" :: String)
+  
+  
